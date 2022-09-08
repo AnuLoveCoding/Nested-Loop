4e820de9c4868a4aacb3131fef0c897c5fe68156
anuLoveCoading <anusrivas07@gmail.com>
2022-09-08 10:18:51 +0530

    01 class nested loop

--- pm1.js
+++ pm1.js
@@ -1,8 +1,20 @@
-console.log("anurag");
-//  chunnu wants to eat 10 gol gappas
+// console.log("anurag");
+// //  chunnu wants to eat 10 gol gappas
 
-for (let f=1;f<=4;f++){
-    for(let gg=1;gg<=10;gg++){
-        console.log(`faimly member number :${f} and gol gappas is ${gg}`)
+// for (let f=1;f<=4;f++){
+//     for(let gg=1;gg<=10;gg++){
+//         console.log(`faimly member number :${f} and gol gappas is ${gg}`)
+//     }
+// }
+
+// rahul ==> chunnu(rahul's son's)
+// rahul has 5 farms
+
+
+// rahul asked chunnu to plant 5 seeds in each farm;
+
+for(let farm=1;farm<=5;farm++){
+    for(let seeds =1;seeds<=5;seeds++){
+        console.log(`farm is ${farm}, seeds is ${seeds}`)
     }
 }
