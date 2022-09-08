950f121d75dfa285f88df454932e3375389adba7
anuLoveCoading <anusrivas07@gmail.com>
2022-09-08 11:32:30 +0530

    04 class

--- pm3.js
+++ pm3.js
@@ -11,5 +11,7 @@ for(let i=1;i<=4;i++){
     }
     console.log(bag1)
     console.log(bag);
+
+    
     
 }
--- pm4.js
+++ pm4.js
@@ -1,8 +1,45 @@
 
 
-let arr =[ "Aman","Ajay","Nurpul","Anurag"]
+let arr =[ "Aman","Ajay","Nurpul","AnurAg"]
    
 
 // for( let arr1 of arr){
 //     console.log(arr1);
 // }
+
+
+// let count =0;
+//   for(let i=0;i<=arr.length-1;i++){
+//     if(arr[i][0]=="N"){
+//         count++
+//     }
+//   }
+//   console.log(count)
+
+
+
+
+
+
+
+
+// let count =0;
+
+// for(let i=0;i<=arr.length-1;i++){
+
+//     for(let j=0;j<=arr[i].length-1;j++){
+
+//         if(arr[i][j]=="A"||arr[i][j]=="N"){
+
+//             count++;
+            
+//         }
+//     }
+// }
+
+// console.log(count);
+   for( let i=1;i<=20;i++){
+    if(i%2===0){
+        console.log(i);
+    }
+   }
