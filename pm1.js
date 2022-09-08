f97ed930ebe1f5e82f63306a71650ca8740c4795
anuLoveCoading <anusrivas07@gmail.com>
2022-09-08 09:58:58 +0530

    01 class

--- pm1.js
+++ pm1.js
@@ -1,1 +1,8 @@
 console.log("anurag");
+//  chunnu wants to eat 10 gol gappas
+
+for (let f=1;f<=4;f++){
+    for(let gg=1;gg<=10;gg++){
+        console.log(`faimly member number :${f} and gol gappas is ${gg}`)
+    }
+}
