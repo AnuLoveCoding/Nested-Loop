e5b0e01b4466113ea5e2f9f3a59f269e620ed5da
anuLoveCoading <anusrivas07@gmail.com>
2022-09-08 10:27:28 +0530

    02 class nested

---
+++ pm2.js
@@ -0,0 +1,10 @@
+//  rahul gain asked his son chunnu to plant  5 seeds in 5 seeds farm, but plant the horizontally;
+
+
+for (let j=1;j<=5;j++){
+     let bag="";
+for(let i=1;i<=5;i++){
+    bag+="*"+" ";
+}
+console.log(bag);
+}
